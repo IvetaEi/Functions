02e97e0f103ee06166f19b4e75b4e9d594253742
IvetaEi <ivetux159@gmail.com>
2024-03-20 16:22:39 +0200

    2 harder exercises

--- src/Main.java
+++ src/Main.java
@@ -72,14 +72,30 @@ public class Main {
         System.out.println("6-tos uzduoties skaiciu vidurkis: " + average);
 
         System.out.println("--------- 9 uzduotis ----------");
-        int ilgis = (int) Math.round(Math.random() * 6);
-        int plotis = (int) Math.round(Math.random() * 4);
-        rectangle(ilgis, plotis);
+        rectangle(4,8);
 
-        System.out.println("--------- 10 uzduotis ---------");
+        System.out.println("\n" + "--------- 10 uzduotis ---------");
         String sentence = "Šiandien labai graži diena";
-        int countSymbols = 0;
-        int countSpaces = 0;
+        sentenceSymbolsCount(sentence);
+
+        System.out.println("--------- 11 uzduotis ---------");
+        String randomName = "Olivia";
+        System.out.println(reverseName(randomName));
+
+        System.out.println("---------- Sunkesni -----------");
+        System.out.println("--------- 1 uzduotis ----------");
+        String text1 = "hello";
+        frontNback(text1);
+
+        System.out.println("--------- 2 uzduotis ----------");
+        int stringLength = 10;
+        String text = generateRndStr(stringLength);
+        System.out.println(text);
+        printTextInAWeirdWay(text);
+
+        System.out.println("--------- 3 uzduotis ----------");
+
+
 
 
   }
@@ -108,9 +124,9 @@ public class Main {
         return (int) (min + Math.round(Math.random() * (max - min)) );
     }
 
-    public static int[] randomArray(int min, int max, int lenght) {
-        int[] array = new int[lenght];
-        for (int i = 0; i < lenght; i++) {
+    public static int[] randomArray(int min, int max, int length) {
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
             array[i] = min + (int) Math.round(Math.random() * (max - min));
             
         }
@@ -134,16 +150,24 @@ public class Main {
         }
         return average;
     }
-
+//    public static void rectangle(int x, int y) {
+//        for (int x1 = 0; x1 <= x; x1++) {
+//            for (int y2 = 0; y2 <= y; y2++) {
+//                if (y2 == y2*4){
+//                    System.out.print(System.lineSeparator());
+//                } else {
+//                    System.out.print("* ");
+//                }
+//            }
+//        }
+//    }
     public static void rectangle(int x, int y) {
-        for (int x1 = 0; x1 <= 6; x1++) {
-            for (int y2 = 0; y2 <= 4; y2++) {
-                if (y2 == y2*4){
-                    System.out.print(System.lineSeparator());
-                } else {
-                    System.out.print("* ");
-                }
+        for(int i=0; i < x; i++) {
+            for(int j=0; j < y; j++)
+            {
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
 //for (int x = 0; x < 26; x++) {
@@ -158,13 +182,85 @@ public class Main {
 //        }
 //
 //    }
-
-
-    public static String sentenceSymbolsCount(String a) {
-
+    public static void sentenceSymbolsCount(String sen) {
+        int countLetters = 0;
+        int countSpaces = 0;
+        for (int i = 0; i < sen.length(); i++) {
+            if (Character.isLetter(sen.charAt(i))) {
+                countLetters++;
+            }else{
+                countSpaces++;
+        }
+      }
+        System.out.println("Is viso raidziu sakinyje: " + countLetters + ", o tarpu: " + countSpaces);
     }
 
-
-
+    public static String reverseName(String name) {
+        String reverse = new StringBuilder(name).reverse().toString();
+        return reverse;
     }
+
+    public static void frontNback(String name) {
+        System.out.println("---" + name + "---");
+    }
+
+    public static String generateRndStr(int length) {
+        String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234567890";
+        String text = "";
+        for (int i = 0; i < length; i++) {
+            text += symbols.charAt((int) (Math.random()*symbols.length()));
+
+        }
+        return text;
+    }
+
+//    public static void printTextInAWeirdWay(String txt){
+//        for (int i = 0; i < txt.length(); i++) {
+//            int countNumbers = 0;
+//            if (txt.charAt(i)+1 == ) {
+//                System.out.println("[" + txt.charAt(i) + "]");}
+//        if (Character.isDigit(txt.charAt(i))) {
+//                System.out.println("[" + txt.charAt(i) + "]");
+//            }else{
+//                System.out.println(txt.charAt(i));
+//            }
+//        }
+//    }
+    public static void printTextInAWeirdWay(String txt) {
+        String number = "";
+        for (int i = 0; i < txt.length(); i++) {
+            char currentChar = txt.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                number += currentChar;
+            }else{
+                if (number.length() > 0) {
+                    System.out.println("[" + number + "]");
+                }
+                System.out.println(currentChar);
+                number = "";
+            }
+        }
+        if (number.length() > 0) {
+        System.out.println("[" + number + "]");}
+    }
+
+//    public static int withoutRem(int number) {
+//        int countWithoutRem = 0;
+//        if (number == 1 || number == number) {
+//            continue;
+//        } else if (number % ) {
+//            countWithoutRem++;
+//        }else{
+//
+//       }
+//        return number;
+//    }
+
+
+
+
+
+
+
+}
 
